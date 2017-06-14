99ae6354d5e1161aa639b29adf693f640f71a271
ProgrammerPanda <gskrishna44@gmail.com>
2017-06-14 13:58:51 -0700

    Create linux.pp

--- manifests/install/linux.pp
+++ manifests/install/linux.pp
@@ -1,5 +1,7 @@
 class filebeat::install::linux {
   package {'filebeat':
+    source => 'rpm',
     ensure => $filebeat::package_ensure,
+    source => 'give the path to your filebeat rpm file"
   }
 }
